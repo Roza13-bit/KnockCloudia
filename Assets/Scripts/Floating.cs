0d25a31e1b7d66393ce556061bc2eee4d8d10051
Luddifix <Orooh147@gmail.com>
2020-11-22 00:50:36 +0200

    Added stages, Added finished global floating script

---
+++ Assets/Scripts/Floating.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floating : MonoBehaviour
+{
+
+    GameObject Disc;
+    public float speed = 1f;
+    public float distance = 1f;
+    public bool FreezeMe = false;
+
+
+    void Update()
+    {
+        if (!FreezeMe)
+        {
+
+            Vector3 move = new Vector3(transform.position.x, Mathf.Sin(speed * Time.time) * distance, transform.position.z);
+            transform.position = move;
+
+        }
+    }
+}
